823ef0e5d5160521c63967a4c835d2221f5701e3
Blxee <aminedev57@gmail.com>
2023-12-27 19:05:09 +0100

    Added: *Longest Common Prefix* solution in c

---
+++ longest-common-prefix/main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+
+
+char* longestCommonPrefix(char** strs, int strsSize)
+{
+  char *prefix = malloc(200 * sizeof(char));
+  unsigned int i, j;
+
+  if (prefix == NULL)
+    return (NULL);
+
+  *prefix = '\0';
+  strcpy(prefix, strs[i]);
+  i = 1;
+  while (i < strsSize)
+  {
+    j = 0;
+    while (prefix[j] &&  prefix[j] == strs[i][j])
+      j++;
+    prefix[j] = '\0';
+    i++;
+  }
+
+  return (prefix);
+}
