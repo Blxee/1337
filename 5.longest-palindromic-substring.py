5665efa8a8754ad17970fd3b7a02cfc148fb41a8
Blxee <aminedev57@gmail.com>
2024-12-27 12:13:17 +0100

    Added: 5 longest palindromic substring solution in Python3

--- 5.longest-palindromic-substring.py
+++ 5.longest-palindromic-substring.py
@@ -1,26 +1,11 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) == 1 or (len(s) == 2 and s[0] == s[1]):
-            return s
-        longest = ''
-        maxlen = 0
-        for i in range(len(s)):
-            for j in reversed(range(i + 1)):
-                if i + j > len(s):
-                    continue
-                substr = s[i - j:i + j + 1]
-                if self.is_palindrome(substr) and len(substr) > maxlen:
-                    longest = substr
-                    maxlen = len(substr)
-                    break
-                substr = s[i - j:i + j]
-                if self.is_palindrome(substr) and len(substr) > maxlen:
-                    longest = substr
-                    maxlen = len(substr)
-                    break
-        return longest
+        for length in range(len(s), 0, -1):
+            for position in range(len(s) - length + 1):
+                substr = s[position:position + length]
+                if self.is_palindrome(substr):
+                    return substr
+        return ""
 
-    def is_palindrome(self, s: str):
-        if len(s) % 2 == 1:
-            s = s[:len(s) // 2] + s[len(s) // 2 + 1:]
-        return s[:len(s) // 2] == s[len(s) // 2:][::-1]
+    def is_palindrome(self, s: str) -> bool:
+        return s == s[::-1]
