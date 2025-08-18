b34e8b2ad1c4490ca8f173afa70450e6580752d8
Blxee <aminedev57@gmail.com>
2025-08-18 08:26:54 +0100

    Changed: 3226

--- 3226.number-of-bit-changes-to-make-two-integers-equal.c
+++ 3226.number-of-bit-changes-to-make-two-integers-equal.c
@@ -2,10 +2,10 @@ int minChanges(int n, int k) {
   int num;
   int mask;
 
-  num = 0;
-  mask = n ^ k;
   if (k & ~n)
     return (-1);
+  num = 0;
+  mask = n ^ k;
   while (mask)
   {
     if (mask & 1)
